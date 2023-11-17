90112627d4a3188829adb16e09a40f53c885347d
Bryan Hauser <vmkid96@gmail.com>
2023-11-17 13:02:45 -0500

    Recreate Home fragment with a loading spinner and debug data.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 buildscript {
-    val kotlin_version by extra("2.0.0-Beta1")
+    val kotlin_version by extra("1.8.10")
     dependencies {
         classpath("com.google.gms:google-services:4.4.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
@@ -10,6 +10,6 @@ buildscript {
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.3" apply false
+    id("com.android.application") version "8.1.4" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
 }
